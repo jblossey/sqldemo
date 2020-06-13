46d32ddf37f6618f1fbb8168b45abc12fe3acb4c
Jannis Blossey <jannis@blossey.eu>
2020-06-13 13:32:02 +0200

    refactor: init for dictionary

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,8 @@
-INSERT INTO "LECTURE" VALUES
-(1000, 'Programmieren in Java'),
-(1001, 'Datenbanksysteme'),
-(1002, 'Anwendungssysteme'),
-(1003, 'Cloud Service Engineering'),
-(1004, 'Praxisprojekt'),
-(1005, 'Grundlagen der Informatik I');
-INSERT INTO "STUDENT" VALUES
-(123456789, 'Maja Musterfrau'),
-(987654321, 'Max Mustermann');
-INSERT INTO "LECTURE_STUDENTS" VALUES
-(1000, 123456789),
-(1001, 123456789),
-(1002, 123456789),
-(1005, 987654321),
-(1004, 987654321),
-(1003, 987654321);
+INSERT INTO "ENGLISH"(word) VALUES
+('hello'),('world'),('sample');
+INSERT INTO "GERMAN"(word) VALUES
+('hallo'),('welt'),('probe');
+INSERT INTO "ENGLISH_GERMAN" VALUES
+(1,1),
+(2,2),
+(3,3);
