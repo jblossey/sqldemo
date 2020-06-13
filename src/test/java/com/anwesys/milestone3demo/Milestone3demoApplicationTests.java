b4d97cf4f9e3d1c9562780dedf1237a84ec1899d
Jannis Blossey <jannis@blossey.eu>
2020-06-13 13:23:52 +0200

    refactor: rename components

--- src/test/java/com/anwesys/milestone3demo/Milestone3demoApplicationTests.java
+++ src/test/java/com/anwesys/milestone3demo/Milestone3demoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.anwesys.sqldemo;
+package com.anwesys.milestone3demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SqldemoApplicationTests {
+class Milestone3demoApplicationTests {
 
 	@Test
 	void contextLoads() {
