00fd3fb076e82391552863e804344123550d66b2
Jannis Blossey <jannis@blossey.eu>
2020-06-14 17:36:50 +0200

    fix: add missing class

--- src/test/java/com/anwesys/milestone3demo/RestApiTests.java
+++ src/test/java/com/anwesys/milestone3demo/RestApiTests.java
@@ -1,6 +1,7 @@
 package com.anwesys.milestone3demo;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
