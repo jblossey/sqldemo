2a858a4a1a996320d06c1587262af8044d3836ff
Jannis Blossey <jannis@blossey.eu>
2020-07-09 11:48:04 +0200

    chore: feature/ch20

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,5 @@ INSERT INTO GERMAN VALUES
 ('hallo'),('welt'),('probe');
 INSERT INTO ENGLISH VALUES
 ('hello','hallo'),('world','welt'),('sample','probe');
+INSERT INTO FRENCH VALUES
+('salut','hallo'),('monde','welt'),('échantillon','probe');
